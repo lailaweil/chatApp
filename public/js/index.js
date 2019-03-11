d398bba5e7645ee44a5078e628be8dd55f19b364
lailaweil <lweil@est.frba.utn.edu.ar>
2019-03-11 19:11:24 -0300

    clean form

--- public/js/index.js
+++ public/js/index.js
@@ -22,7 +22,7 @@ jQuery('#message-form').on('submit', function(e){
         from: 'User',
         text: jQuery('[name=message]').val()
     }, function(){
-
+      jQuery('[name=message]').val('');
     });
 });
 
