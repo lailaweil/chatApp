8126ffac457ce4f604e90eb2e44171daaae0a5e3
lailaweil <lweil@est.frba.utn.edu.ar>
2019-03-11 14:49:54 -0300

    added function to make message

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
+const admin = 'Admin';
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
@@ -15,25 +18,14 @@ io.on('connection', (socket) => {
     console.log('Nuevo usuario conectado.');
 
     //solo al socket actual si quiero a todos es io.emit
-    socket.emit('newMessage',{
-        from: 'Admin',
-        text: '¡Bienvenido al chat!',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage(admin,'¡Bienvenido al chat!'));
 
-    socket.broadcast.emit('newMessage',{
-        from: 'Admin',
-        text: 'Nuevo usuario se ha unido',
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage',generateMessage(admin, 'Nuevo usuario se ha unido.'));
 
     socket.on('createMessage', (mes)=>{
         console.log('Mensaje creado: ', mes);
-        io.emit('newMessage', {
-            from: mes.from,
-            text: mes.text,
-            createdAt: new Date().getTime()
-        });
+
+        io.emit('newMessage', generateMessage(mes.from,mes.text));
 
         // socket.broadcast.emit('newMessage', {
         //         from: mes.from,
