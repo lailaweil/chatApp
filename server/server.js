eb1f4ca29df73722f3e60fe0b109747aef2992fe
lailaweil <lweil@est.frba.utn.edu.ar>
2019-03-15 12:17:00 -0300

    location emit changes

--- server/server.js
+++ server/server.js
@@ -43,7 +43,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+
+    if(user){
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
